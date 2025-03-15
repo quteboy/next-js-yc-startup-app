e9f571a53bd629a525feca18c56bd105fa707af0
Zaid <zaidshaikh460@gmail.com>
2025-03-15 15:28:04 +0530

    nextjs 15 app oauth has been setup with git

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="text-2xl">Home</h2>
+    </>
+  );
+}
---
+++ auth.ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth"
+import GitHub from "@auth/core/providers/github";
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+})
