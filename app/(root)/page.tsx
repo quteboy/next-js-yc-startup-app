249f8d5546f050d96b7884389b8090d2862a91a1
Zaid <zaidshaikh460@gmail.com>
2025-03-17 12:36:27 +0530

    added the searchform using React-form and developed a server component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({searchParams,}: {
+  searchParams: Promise<{ query?: string }>;}) {
+  const query = (await searchParams).query
   return (
     <>
       <section className="pink_container">
@@ -8,8 +12,10 @@ export default function Home() {
           Connect with Entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
